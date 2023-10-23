fc37b8171bb0d105dca2ec4c80ca8cbe54c66b88
Pokoknyabagus <bagussugiono90@gmail.com>
2023-10-23 18:53:15 +0700

    pembaharuan portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#facc15',
+        secondary: '#9ca3af'
+      },
+    },
+  },
+  plugins: [],
+}
+
